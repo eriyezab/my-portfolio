e6c0fe5ab64e4ad4974004eb05063b38f0b53e5f
Eriyeza <buwembo.eriyeza@gmail.com>
2020-06-02 21:56:42 +0000

    Create comment class that will represent comments made by users.

---
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,38 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.data;
+
+/**
+ * Class representing a comment that t users create.
+ *
+ * <p>Note: The private variables in this class are converted into JSON.
+ */
+public class Comment {
+
+  /** The name of the user who commented. */
+  private String name;
+
+  /** The message that the user commented. */
+  private String message;
+
+  /** The time the user made the comment. */
+  private long timestamp;
+  
+  public Comment(String name, String message, long timestamp) {
+    this.name = name;
+    this.message = message;
+    this.timestamp = timestamp;
+  }
+}
