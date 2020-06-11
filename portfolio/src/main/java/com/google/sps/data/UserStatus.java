0e4104919c13dcb4ca2e580781f09db0545f1918
Eriyeza <buwembo.eriyeza@gmail.com>
2020-06-11 14:47:09 +0000

    Update class javadoc and change attributand getter name to better reflect functionality.

--- portfolio/src/main/java/com/google/sps/data/UserStatus.java
+++ portfolio/src/main/java/com/google/sps/data/UserStatus.java
@@ -17,23 +17,25 @@ package com.google.sps.data;
 /**
  * Class representing the logged in status of the user.
  *
- * <p>Note: The private variables in this class are converted into JSON.
+ * <p>Note: The private variables in this class are converted into JSON and sent to the
+ * client in order to determine whether to show them the comment form and a log out 
+ * link if isLogged in is true or to show them a log in link and hide the comment form if false.
  */
 public class UserStatus {
 
   /** The logged in status of the user */
-  private boolean loggedIn;
+  private boolean isLoggedIn;
 
   /** The url that the user clicks to log in or log out depending on status. */
   private String url;
 
   public UserStatus(boolean loggedIn, String url) {
-    this.loggedIn = loggedIn;
+    this.isLoggedIn = loggedIn;
     this.url = url;
   }
   
-  public boolean getLoggedIn() {
-    return this.loggedIn;
+  public boolean isLoggedIn() {
+    return this.isLoggedIn;
   }
 
   public String getUrl() {
